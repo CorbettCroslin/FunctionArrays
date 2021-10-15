8ca48e43fdbfdb72f6e205f81b9c6ced8a9eab31
Corbett <corbett.croslin@gmail.com>
2021-10-15 16:47:42 -0400

    add

---
+++ app.js
@@ -0,0 +1,38 @@
+// 2. Create a function that returns the sum of two numbers that are arguments. Then console.log the function with the variables from step two as your two arguments.
+
+// 3. Create a function that returns the difference of two numbers that are arguments. Then console.log the function with the variables from step two as your two arguments.
+
+// 4. Create a function that returns two arguments multiplied by each other. Then console.log the function with the variables from step two as your two arguments.
+
+// 5. Create a function that returns divides the first argument by the second argument and returns the new value. Then console.log the function with the variables from step two as your two arguments.
+
+
+
+function getSum (a,b) 
+    console.log(a + b);
+    
+}
+
+getSum(2,4);
+
+function getSub (c,d) {
+    console.log(c - d);
+   
+}
+
+getSub(6,8);
+
+
+function getMul (e,f) {
+    console.log(e * f);
+
+}
+
+getMul(10,12);
+
+function getDiv (g,h) {
+    console.log(g / h);
+  
+}
+
+getDiv(14,16);
